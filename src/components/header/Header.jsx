2439593d7fc874b94c6467019179fe018bd7c586
iamLolade <adeniyiotemade@gmail.com>
2022-11-03 16:05:28 +0100

    Updated Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
-import { StyledHeader } from "../styles/Header.styled";
+import { StyledHeader, Image } from "../styles/Header.styled";
+import { Flex } from "../styles/Flex.styled";
+import { Input } from 'antd';
 
 const Header = () => {
     return (
         <StyledHeader>
-            T
+            <Flex>
+                <div>
+                    <Image src="./icons/arrow-left.svg" alt="Left arrow" />
+                    <Image src="./icons/arrow-right.svg" alt="Right arrow" />
+                </div>
+                <Input size='large' />
+            </Flex>
         </StyledHeader>
     );
 }
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -2,8 +2,12 @@ import styled from 'styled-components'
 
 export const StyledHeader = styled.div`
     display: flex;
-    height: 15%;
+    height: 10%;
     background: #000;
     padding: 0 20px;
     margin: 0 auto;
+`
+
+export const Image = styled.img`
+  margin-right: 10px;
 `
